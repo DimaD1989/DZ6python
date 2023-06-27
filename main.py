e5b4ef84b9dfb6c83cb97da36d8e091769dcbcb6
DimaD1989 <d_d1989sv@mail.ru>
2023-06-27 19:21:27 +0300

    end task 2

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 # В модуль с проверкой даты добавьте возможность запуска в терминале с передачей даты на проверку.
 
-from datetime import datetime
+# from datetime import datetime
+#
+# def date_chek(user_date:str):
+#     try:
+#         val = datetime.strptime(user_date, "%d.%m.%Y").date()
+#         return True
+#     except:
+#         return False
+#
+# if __name__ == '__main__':
+#
+#
+#     print(date_chek(input("Введите дату в формате(dd.mm.YYYY) -  ")))
 
-def date_chek(user_date:str):
-    try:
-        val = datetime.strptime(user_date, "%d.%m.%Y").date()
-        return True
-    except:
-        return False
+# Добавьте в пакет, созданный на семинаре шахматный модуль.
+# Внутри него напишите код, решающий задачу о 8 ферзях.
+# Известно, что на доске 8×8 можно расставить 8 ферзей так, чтобы они не били друг друга.
+# Вам дана расстановка 8 ферзей на доске, определите, есть ли среди них пара бьющих друг друга.
+# Программа получает на вход восемь пар чисел, каждое число от 1 до 8 - координаты 8 ферзей.
+# Если ферзи не бьют друг друга верните истину, а если бьют - ложь.
 
-if __name__ == '__main__':
+n = 8
+x = []
+y = []
+for i in range(n):
+    new_x, new_y = [int(s) for s in input().split()]
+    x.append(new_x)
+    y.append(new_y)
 
+correct = True
+for i in range(n):
+    for j in range(i + 1, n):
+        if x[i] == x[j] or y[i] == y[j] or abs(x[i] - x[j]) == abs(y[i] - y[j]):
+            correct = False
 
-    print(date_chek(input("Введите дату(dd.mm.YYYY) -  ")))
+if correct:
+    print('Ферзи  бьют друг друга')
+else:
+    print('Ферзи не бьют друг друга')
